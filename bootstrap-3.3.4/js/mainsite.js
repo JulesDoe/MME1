c285ccc75932e2eec7fcb22c7d81bc9c593e2642
DerCodeHahn <derhahn91@web.de>
2015-07-07 12:56:19 +0200

    [Database] Added Database

--- bootstrap-3.3.4/js/mainsite.js
+++ bootstrap-3.3.4/js/mainsite.js
@@ -4,7 +4,7 @@ $(function(){
                 console.log(resp);
                 var objects =   jQuery.parseJSON(resp);
                 $.each(objects, function(serienr) {
-                            var html = '<div class="row">';
+                            var html = '<div class="row box">';
                                 html += '<img class="col-md-3" src="../' + objects[serienr]["imgsource"] + '" alt="dexter"/>';
                                 html += '<h2 class="col-md-9">';
                                 html += objects[serienr]["name"];
@@ -12,10 +12,23 @@ $(function(){
                                 html += '<p class="col-md-9">';
                                 html += objects[serienr]["beschreibung"];
                                 html += '</p>';
+                                var staffeln = objects[serienr][""];
+                                var watched = 0;
+                                var fulllength = 0;
+                                for(var i = 0; i <= staffeln.length; i++)
+                                {
+                                    for(var a = 0; a <= staffeln[i].length; a++)
+                                    {
+                                        if(staffeln[i][a] == 1)
+                                            watched++;
+                                        fulllength++;
+                                    }
+                                }
+                                html += '<progressbar';
                                 html += '</div>';
                                 $('#content').append(html);
 
-                                console.log(html);
+                                //console.log(html);
                         });
             });
 });
